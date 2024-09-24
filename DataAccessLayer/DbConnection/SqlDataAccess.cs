f8d8f2c1c74c94e8e86f7fc230db87f090827883
androso <androso421@gmail.com>
2024-09-24 14:47:53 -0600

    init repo

---
+++ DataAccessLayer/DbConnection/SqlDataAccess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.DbConnection
+{
+    public class SqlDataAccess
+    {
+        private readonly string _connectionString;
+
+        public SqlDataAccess()
+        {
+             _connectionString = "Data Source=ARTEMUS\\SQLEXPRESS;Initial Catalog=StudentAdministratorDB;Integrated Security=True;Encrypt=False";
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
